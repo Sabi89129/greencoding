01f59f07f9a00f4b622fc1b36426ead9414caea7
Sabi89129 <62985283+Sabi89129@users.noreply.github.com>
2022-08-05 17:08:05 +0200

    Update erste_uebung.py

--- erste_uebung.py
+++ erste_uebung.py
@@ -12,8 +12,3 @@ for x in range(1000000000):
 print(' Hallo Informatica feminale 2022')
 
 tracker.stop()
-
-
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = [x for x in fruits if "a" in x]
-print(newlist) 
